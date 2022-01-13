59eee4b5c10f2d27d31d99e033c1d4e92f891cd2
Harsh kumar <97426451+harshnitj@users.noreply.github.com>
2022-01-13 18:01:16 +0530

    Update Api.java

--- app/src/main/java/com/app/pnrcheck/Api.java
+++ app/src/main/java/com/app/pnrcheck/Api.java
@@ -7,8 +7,8 @@ import retrofit2.http.Path;
 
 public interface Api {
     @Headers({
-            "x-rapidapi-host:pnr-status-indian-railway.p.rapidapi.com",
-            "x-rapidapi-key:d4dc795e8fmshb698dcb29bf3003p1b1715jsn4bbcebe7a88a"
+            "",
+            ""
     })
     @GET("{pnr}")
 
